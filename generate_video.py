b644b7add86df4b9c64be06a6a01acd38af6b1da
Sgt-cod <asael.carvalho@gmail.com>
2025-12-13 12:25:03 -0300

    Update generate_video.py

--- generate_video.py
+++ generate_video.py
@@ -470,7 +470,7 @@ return
     
    # thumbnail_path = f'{VIDEOS_DIR}/thumb_{timestamp}.jpg'
   #  criar_thumbnail(titulo_video, thumbnail_path, VIDEO_TYPE)
-    thumbnail_path = None
+thumbnail_path = None
     
     titulo = titulo_video[:60] if len(titulo_video) <= 60 else titulo_video[:57] + '...'
     if VIDEO_TYPE == 'short':
